6ff480614555d0c67429c0ab6d38498f5b3d022b
Linkova Tanya <Linkova Tanya@DESKTOP-BGMEJG7>
2021-03-27 22:32:41 +0300

    Add project files.

---
+++ Json/Json.cpp
@@ -0,0 +1,58 @@
+﻿
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
+#include <istream>
+#include <locale>
+#include <ostream>
+#include <iostream>
+
+#include "./json.hpp"
+
+using nlohmann::json;
+
+int main(int argc, char** argv)
+{
+	//std::filesystem::path path_input = "input.txt";
+	std::filesystem::path path_output = "output.txt";
+
+	//std::fstream fin(path_input.string(), std::ios::in);
+
+	json j;
+	/*fin >> j;
+	fin.close();*/
+	std::string name, surname;
+	int age;
+	//std::cout << std::setw(3) << j << std::endl;
+	std::cin >> name;
+	std::cin >> surname;
+	std::cin >> age;
+	j["name"] = name;
+	j["surname"] = surname;
+	j["age"] = age;
+	/*for (auto iterator = j.begin(); iterator != j.end(); ++iterator)
+	{
+		std::cout << iterator.key() << " : " << iterator.value() << "\n";
+	}
+	std::cout << "\n";
+
+	for (auto& element : j.items())
+	{
+		std::cout << element.key() << " : " << element.value() << "\n";
+	}
+	std::cout << "\n";*/
+
+	for (auto& [key, value] : j.items()) // C++17 bindings
+	{
+		std::cout << key << " : " << value << "\n";
+	}
+	std::cout << "\n";
+
+	std::fstream fout(path_output.string(), std::ios::out);
+	fout << std::setw(3) << j;
+	fout.close();
+
+	system("pause");
+
+	return EXIT_SUCCESS;
+}
